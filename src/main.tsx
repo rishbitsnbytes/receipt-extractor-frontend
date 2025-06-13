ea21069c416071867d12dce55a72194827690b75
rishbitsnbytes <rishrathore1613@gmail.com>
2025-06-13 19:06:16 +0530

    feat: initial setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App title="Hello World" blurb="You can modify this project as you like to complete your assessment." />
+    <App />
   </StrictMode>,
 )
